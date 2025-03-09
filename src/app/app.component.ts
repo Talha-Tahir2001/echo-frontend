4b6360c2ac7434b9e35aba1bee9d2b339bc2f21d
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-03-10 02:02:11 +0500

    update: App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { UserService } from './services/user.service';
 
 
 @Component({
@@ -7,8 +8,31 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styles: [],
+  styles: [`
+    @keyframes wave {
+  0%, 100% { transform: translateY(0); }
+  50% { transform: translateY(-3px); }
+}
+
+.wave-animation {
+  display: inline-block;
+  animation: wave 1.5s infinite ease-in-out;
+}
+    `],
 })
 export class AppComponent {
   title = 'echo-frontend';
+  userService : UserService = inject(UserService);
+  constructor(){
+    const user = this.userService.getUserFromStorage();
+    if(!user){
+      const randomNumber = Math.ceil(Math.random() * 7000 + 1000);
+      const randomName = `user_${randomNumber}`;
+      this.userService.createUser(randomName)
+      .subscribe(user => {
+        console.log('User Created', user);
+        this.userService.saveUserToStorage(user);
+      })
+    }
+  }
 }
