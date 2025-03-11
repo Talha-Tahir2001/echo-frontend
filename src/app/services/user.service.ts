3cb88990ad773ca35e93f043de0efb343596bd4c
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-03-12 00:33:59 +0500

    add: User Service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { User } from '../interfaces/user.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+  http : HttpClient = inject(HttpClient);
+  localStorageKey = 'echo_user';
+
+  createUser(name:string){
+    return this.http.post<User>(`${environment.PROD_BACKEND_URL}/users`, {
+      name,
+    });
+  }
+
+  saveUserToStorage(user:User){
+    localStorage.setItem(this.localStorageKey, JSON.stringify(user));
+  }
+  getUserFromStorage(){
+    const user = localStorage.getItem(this.localStorageKey);
+    return user ? (JSON.parse(user) as User) : null;
+  }
+}
