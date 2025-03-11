936ecf5eac5036605db8f7df5e604222658543fe
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-03-11 23:40:31 +0500

    add: Home page

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,43 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { CommentComponent } from '../components/comment/comment.component';
+import { CommentService } from '../services/comment.service';
+import { Comment } from '../interfaces/comment.interface';
+import { CommentFormComponent } from '../components/comment-form/comment-form.component';
+import { UserService } from '../services/user.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommentComponent, CommentFormComponent],
+  templateUrl: './home.component.html',
+  styles: ``,
+})
+export class HomeComponent implements OnInit {
+  comments = signal<Comment[]>([]);
+  commentService: CommentService = inject(CommentService);
+  userService: UserService = inject(UserService);
+
+  createComment(formValues: { text: string }) {
+    const { text } = formValues;
+    const user = this.userService.getUserFromStorage();
+    if (!user) return;
+    this.commentService
+      .createComment({
+        text,
+        userId: user._id,
+      })
+      .subscribe((createdComment) => {
+        this.comments.set([createdComment, ...this.comments()]);
+      });
+  }
+
+  ngOnInit(): void {
+    this.getComments();
+  }
+
+  getComments() {
+    this.commentService.getComments().subscribe((comments) => {
+      this.comments.set(comments);
+    });
+  }
+}
