8e8da6c74c1df5fe11dce58e4df534f20339476d
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-03-12 00:33:42 +0500

    add: Comment Service

---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Comment } from '../interfaces/comment.interface';
+
+type CreateCommentDto = {
+  parentId?: string;
+  text: string;
+  userId: string;
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CommentService {
+  constructor() {}
+
+  http = inject(HttpClient);
+
+  getComments(parentId: string = '') {
+    let url = `${environment.PROD_BACKEND_URL}/comments`;
+    if (parentId) {
+      url += `?parentId=${parentId}`;
+    }
+    return this.http.get<Comment[]>(url);
+  }
+
+  createComment(comment: CreateCommentDto) {
+    return this.http.post<Comment>(
+      `${environment.PROD_BACKEND_URL}/comments`,
+      comment
+    );
+  }
+}
